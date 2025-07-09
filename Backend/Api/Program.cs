fa390052a6805142a1c8d44e0dc49b6d39a0ed8d
N1ghtmarex <andreyshe22@gmail.com>
2025-07-09 20:19:54 +0700

    Настроена интеграция с Keycloak

--- Backend/Api/Program.cs
+++ Backend/Api/Program.cs
@@ -1,13 +1,31 @@
+using Api.StartupConfigurations;
+using Api.StartupConfigurations.Options;
 using Domain;
+using Keycloak.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 builder.Services.RegisterDataAccessService(builder.Configuration);
+
+builder.Services.ConfigureOptions<KeycloakConfigurationSetup>();
+builder.Services.ConfigureOptions<KeycloakScopesConfigurationSetup>();
+
+builder.Services.AddKeycloakConfiguration();
+builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
 
 app.SetConnectionStringEnvironment(app.Configuration.GetConnectionString("DbConnection"));
 
+app.MapControllers();
+
 app.MigrateDb();
+
+app.ConfigureSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
