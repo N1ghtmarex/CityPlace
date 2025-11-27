da05667db8dff3e079abba8d774d6d5a3f81a062
N1ghtmarex <andreyshe22@gmail.com>
2025-11-27 15:50:39 +0700

    feat: Добавлены мапперы

---
+++ Backend/Application/Mappers/AddressMapper.cs
@@ -0,0 +1,12 @@
+﻿using Application.Addresses.Dtos;
+using Domain.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace Application.Mappers;
+
+[Mapper]
+public static partial class AddressMapper
+{
+    public static partial IQueryable<AddressViewModel> ProjectToViewModel(this IQueryable<Address> q);
+    public static partial AddressViewModel MapToViewModel(Address q);
+}
---
+++ Backend/Application/Mappers/LocationMapper.cs
@@ -0,0 +1,24 @@
+﻿using Application.Addresses.Dtos;
+using Application.Locations.Dtos;
+using Domain.Entities;
+using Domain.Enums;
+using Riok.Mapperly.Abstractions;
+
+namespace Application.Mappers;
+
+[Mapper]
+public static partial class LocationMapper
+{
+    public static partial IQueryable<LocationViewModel> ProjectToViewModels(this IQueryable<Location> q, AddressViewModel address);
+    public static partial IQueryable<LocationListViewModel> ProjectToListViewModels(this IQueryable<Location> q, AddressViewModel address);
+
+    [MapValue(nameof(Location.Id), Use = nameof(GenerateId))]
+    [MapValue(nameof(Location.CreatedAt), Use = nameof(SetCreatedAt))]
+    public static partial Location MapToEntity(CreateLocationModel source, Address address, LocationType type);
+
+    [NamedMapping("GenerateId")]
+    static Ulid GenerateId() => Ulid.NewUlid();
+
+    [NamedMapping("SetCreatedAt")]
+    static DateTimeOffset SetCreatedAt() => DateTime.UtcNow;
+}
