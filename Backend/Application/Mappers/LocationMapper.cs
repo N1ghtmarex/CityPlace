d1f6e6bf2e0877fbdc878cc4c9c1d1511068865b
N1ghtmarex <andreyshe22@gmail.com>
2025-11-27 17:13:53 +0700

    fix: Исправлен вызов методов генерации полей

--- Backend/Application/Mappers/LocationMapper.cs
+++ Backend/Application/Mappers/LocationMapper.cs
@@ -14,7 +14,7 @@ public static partial class LocationMapper
     public static partial IQueryable<LocationViewModel> ProjectToViewModel(this IQueryable<Location> q);
     public static partial IQueryable<LocationListViewModel> ProjectToListViewModels(this IQueryable<Location> q);
 
-    [MapValue(nameof(Location.Id), Use = nameof(GeneralMapper.GenerateId))]
-    [MapValue(nameof(Location.CreatedAt), Use = nameof(GeneralMapper.SetCreatedAt))]
+    [MapValue(nameof(Location.Id), Use = nameof(@GeneralMapper.GenerateId))]
+    [MapValue(nameof(Location.CreatedAt), Use = nameof(@GeneralMapper.SetCreatedAt))]
     public static partial Location MapToEntity(CreateLocationModel source, Address address, LocationType type);
 }
