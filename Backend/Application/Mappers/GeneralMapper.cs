bc4a8afa3153b3b3992b783ae183c7a7e7fce159
N1ghtmarex <andreyshe22@gmail.com>
2025-11-27 16:53:56 +0700

    feat: Добавлен общий маппер + Добавлен общий маппер * NamedMapping вынесены в общий маппер

---
+++ Backend/Application/Mappers/GeneralMapper.cs
@@ -0,0 +1,19 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace Application.Mappers;
+
+[Mapper]
+public static partial class GeneralMapper
+{
+    [NamedMapping("GenerateId")]
+    public static Ulid GenerateId()
+    {
+        return Ulid.NewUlid();
+    }
+
+    [NamedMapping("SetCreatedAt")]
+    public static DateTimeOffset SetCreatedAt()
+    {
+        return DateTime.UtcNow;
+    }
+}
--- Backend/Application/Mappers/LocationMapper.cs
+++ Backend/Application/Mappers/LocationMapper.cs
@@ -7,25 +7,14 @@ using Riok.Mapperly.Abstractions;
 namespace Application.Mappers;
 
 [Mapper]
+[UseStaticMapper(typeof(GeneralMapper))]
 [UseStaticMapper(typeof(AddressMapper))]
 public static partial class LocationMapper
 {
     public static partial IQueryable<LocationViewModel> ProjectToViewModel(this IQueryable<Location> q);
     public static partial IQueryable<LocationListViewModel> ProjectToListViewModels(this IQueryable<Location> q);
 
-    [MapValue(nameof(Location.Id), Use = nameof(GenerateId))]
-    [MapValue(nameof(Location.CreatedAt), Use = nameof(SetCreatedAt))]
+    [MapValue(nameof(Location.Id), Use = nameof(GeneralMapper.GenerateId))]
+    [MapValue(nameof(Location.CreatedAt), Use = nameof(GeneralMapper.SetCreatedAt))]
     public static partial Location MapToEntity(CreateLocationModel source, Address address, LocationType type);
-
-    [NamedMapping("GenerateId")]
-    private static Ulid GenerateId()
-    {
-        return Ulid.NewUlid();
-    }
-
-    [NamedMapping("SetCreatedAt")]
-    private static DateTimeOffset SetCreatedAt()
-    {
-        return DateTime.UtcNow;
-    }
 }
