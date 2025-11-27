d774483a2418e898dcfaec7566cf44564f649813
N1ghtmarex <andreyshe22@gmail.com>
2025-11-27 17:23:54 +0700

    feat: Добавлен маппер + Добавлен маппер для сущности User

---
+++ Backend/Application/Mappers/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using Application.Users.Dtos;
+using Domain.Entities;
+using Domain.Enums;
+using Keycloak.Models.KeycloakApiModels;
+using Riok.Mapperly.Abstractions;
+
+namespace Application.Mappers;
+
+[Mapper]
+public static partial class UserMapper
+{
+    public static partial CreateKeyckloakUserModel MapToKeycloakUser(CreateUserModel source, List<Credentials> credentials);
+    public static partial User MapToEntity(CreateUserModel source, Guid externalUserId, UserRole role);
+    public static partial IQueryable<UserViewModel> ProjectToViewModel(this IQueryable<User> q);
+    public static partial IQueryable<UserListViewModel> ProjectToListViewModel(this IQueryable<User> q);
+}
