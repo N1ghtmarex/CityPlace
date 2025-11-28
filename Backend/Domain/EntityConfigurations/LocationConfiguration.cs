70d35649ca7ee1c325cf1859c097d802539ca64b
N1ghtmarex <andreyshe22@gmail.com>
2025-11-28 19:39:10 +0700

    refactor: изменена конфигурация сущности Location + Добавлены индексы для полей Id, Type, CreatedAt, UpdatedAt, IsArchive + Добавлено явное указание уникальности для поля Id + Добавлено ограничение максимальной длины названия локации в 50 символов + Добавлено ограничение максимальной длины описания локации в 100 символов + Выполнена миграция

--- Backend/Domain/EntityConfigurations/LocationConfiguration.cs
+++ Backend/Domain/EntityConfigurations/LocationConfiguration.cs
@@ -15,20 +15,28 @@ namespace Domain.EntityConfigurations
                 .HasConversion(
                     x => x.ToString(),
                     x => Ulid.Parse(x));
+            builder.HasIndex(x => x.Id).IsUnique();
 
-            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
             builder.HasIndex(x => x.Name).IsUnique();
 
-            builder.Property(x => x.Description).IsRequired(false);
+            builder.Property(x => x.Description).IsRequired(false).HasMaxLength(100);
+
             builder.Property(x => x.Type).IsRequired();
+            builder.HasIndex(x => x.Type).IsUnique(false);
+
             builder.Property(x => x.Latitude).IsRequired();
             builder.Property(x => x.Longitude).IsRequired();
-
             builder.HasIndex(x => new { x.Latitude, x.Longitude }).IsUnique(false);
 
             builder.Property(x => x.CreatedAt).IsRequired();
+            builder.HasIndex(x => x.CreatedAt).IsUnique(false);
+
             builder.Property(x => x.UpdatedAt).IsRequired(false);
+            builder.HasIndex(x => x.UpdatedAt).IsUnique(false);
+
             builder.Property(x => x.IsArchive).IsRequired();
+            builder.HasIndex(x => x.IsArchive).IsUnique(false);
         }
     }
 }
